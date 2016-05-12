b6784cdc3d146e3c9780064a8144db6f13ae34d9
Nelson Jeppesen <nelson@nelsonjeppeson-mbp.local>
2016-05-12 12:12:08 -0700

    Add some hammerspoon ui settings

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -1,5 +1,5 @@
 cask 'hammerspoon' do
-  version '0.1'
+  version '0.2'
   url 'https://github.com/Jeppesen-io/homebrew-hammerspoon/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-hammerspoon'
   sha256 :no_check
@@ -7,5 +7,18 @@ cask 'hammerspoon' do
   artifact 'homebrew-hammerspoon-master/dot-files', target: "#{ENV['HOME']}/.hammerspoon"
 
   depends_on cask: 'hammerspoon'
+
+  postflight do
+
+    # Dont show in dock
+    defaults write org.hammerspoon.Hammerspoon MJShowDockIconKey -int 0
+
+    # show in menu
+    defaults write org.hammerspoon.Hammerspoon MJShowMenuIconKey -int 1
+
+    # Dont autoupdate
+    defaults write org.hammerspoon.Hammerspoon SUEnableAutomaticChecks -int 0
+
+  end
 
 end
