64f80edc5d2d0b2518bfb0c54fb9bee2ba3bbc63
Nelson Jeppesen <nelson@simplymeasured.com>
2017-05-19 11:34:29 -0700

    Bump to 1.4.2

--- Casks/my-hammerspoon.rb
+++ Casks/my-hammerspoon.rb
@@ -1,6 +1,6 @@
 cask 'my-hammerspoon' do
 
-  version '1.4.1'
+  version '1.4.2'
   url 'https://github.com/Jeppesen-io/homebrew-hammerspoon/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-hammerspoon'
   sha256 :no_check
