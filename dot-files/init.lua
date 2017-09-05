c69e2197e2d7694bd1c00ca006d394450f299190
Nelson Jeppesen <nelson>
2017-09-05 14:29:03 -0700

    Formating

--- dot-files/init.lua
+++ dot-files/init.lua
@@ -19,18 +19,25 @@ bindAppToKey(false, 's', 'Rambox')
 bindAppToKey(false, 'd', 'iTerm')
 bindAppToKey(false, 'm', 'Spotify')
 bindAppToKey(false, 'a', 'Visual Studio Code')
+
 -- Sleep display/computer
 hs.hotkey.bind(metaKey, 'delete', function() hs.execute('pmset displaysleepnow')  end)
+
 -- Reload hammerspoon config
 hs.hotkey.bind(metaKey, 'r', function() hs.reload() end)
+
 -- fill screen
 hs.hotkey.bind(metaKey, 'Up',     moveWindowPercentOfScreen(0.0,  0.0,  1.0,  1.0))
+
 -- bottom 20%
 hs.hotkey.bind(metaKey, 'Down',   moveWindowPercentOfScreen(0.0,  0.8,  1.0,  1.0))
+
 -- left 60%
 hs.hotkey.bind(metaKey, 'Left',   moveWindowPercentOfScreen(0.0,  0.0,  0.6,  1.0))
+
 -- right 40%
 hs.hotkey.bind(metaKey, 'Right',  moveWindowPercentOfScreen(0.6,  0.0,  1.0,  1.0))
+
 ---- Move monitors
 hs.hotkey.bind(superMetaKey,   'Left',   moveMonitor() )
 hs.hotkey.bind(superMetaKey,   'Right',  moveMonitor() )
