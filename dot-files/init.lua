2041fbba65d0037479422b78878001a27d64e95c
Nelson Jeppesen <nelson>
2017-09-05 14:26:20 -0700

    Some cleanup, move iterm to left side of KB

--- dot-files/init.lua
+++ dot-files/init.lua
@@ -8,27 +8,29 @@ end
 hs.window.animationDuration = 0
 
 -- Set base key combo
-metaKey = {"ctrl","cmd"}
+metaKey      = {'ctrl','cmd'}
+superMetaKey = {'ctrl','cmd','option'}
 
 -- Lauch or focus app by key
 bindAppToKey(false, 'c', 'Google Chrome')
 bindAppToKey(false, 'v', 'Google Chrome Canary')
 bindAppToKey(false, 'p', 'Safari')
 bindAppToKey(false, 's', 'Rambox')
-bindAppToKey(false, 'i', 'iTerm')
+bindAppToKey(false, 'd', 'iTerm')
 bindAppToKey(false, 'm', 'Spotify')
 bindAppToKey(false, 'a', 'Visual Studio Code')
-
 -- Sleep display/computer
 hs.hotkey.bind(metaKey, 'delete', function() hs.execute('pmset displaysleepnow')  end)
-
 -- Reload hammerspoon config
 hs.hotkey.bind(metaKey, 'r', function() hs.reload() end)
-
---- Move window to left/right/top or bottom half of the screen
-hs.hotkey.bind(metaKey, "Up",     moveWindowPercentOfScreen(0.0,  0.0,  1.0,  1.0))
-hs.hotkey.bind(metaKey, "Down",   moveWindowPercentOfScreen(0.0,  0.0,  1.0,  1.0))
-
+-- fill screen
+hs.hotkey.bind(metaKey, 'Up',     moveWindowPercentOfScreen(0.0,  0.0,  1.0,  1.0))
+-- bottom 20%
+hs.hotkey.bind(metaKey, 'Down',   moveWindowPercentOfScreen(0.0,  0.8,  1.0,  1.0))
+-- left 60%
+hs.hotkey.bind(metaKey, 'Left',   moveWindowPercentOfScreen(0.0,  0.0,  0.6,  1.0))
+-- right 40%
+hs.hotkey.bind(metaKey, 'Right',  moveWindowPercentOfScreen(0.6,  0.0,  1.0,  1.0))
 ---- Move monitors
-hs.hotkey.bind(metaKey, "Left",   moveWindowPercentOfScreen(0.0,  0.0,  0.6,  1.0))
-hs.hotkey.bind(metaKey, "Right",  moveWindowPercentOfScreen(0.6,  0.0,  1.0,  1.0))
+hs.hotkey.bind(superMetaKey,   'Left',   moveMonitor() )
+hs.hotkey.bind(superMetaKey,   'Right',  moveMonitor() )
